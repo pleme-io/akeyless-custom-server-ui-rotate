43faf8393bebf0a7561838b563b56b505792bc8b
drzzln <drzzln@protonmail.com>
2026-03-15 21:10:06 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -3,19 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      # NOTE: buildNpmPackage fails because package-lock.json references a private
-      # npm registry that requires TLS certificates unavailable in the Nix sandbox.
-      # Build with: npm install && node index.js
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ nodejs_22 ];
-      };
-    });
+  outputs = inputs:
+    (import "${inputs.substrate}/lib/repo-flake.nix" {
+      inherit (inputs) nixpkgs flake-utils;
+    }) {
+      self = inputs.self;
+      language = "shell";
+      description = "Node.js service for automating credential rotation on websites using the Akeyless Custom Rotator with Puppeteer";
+    };
 }
